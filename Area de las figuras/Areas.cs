329e30c751cc57088d661aa23257fb175c23b140
MARTIN CORTEZ LOPEZ <I20050008@monclova.tecnm.mx>
2020-12-08 17:30:47 -0600

    sakdasdasjkdaskja

--- Area de las figuras/Areas.cs
+++ Area de las figuras/Areas.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Management.Instrumentation;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Area_de_las_figuras
 {
